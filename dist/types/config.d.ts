e72e7d372fd82783928a76fbd8481135e88378b0
Nat Budin <nbudin@actbluetech.com>
2023-12-01 13:42:18 -0800

    Build and check in the dist files just so I can temporarily get this working in an app

---
+++ dist/types/config.d.ts
@@ -0,0 +1,21 @@
+export declare const ConnectElementCustomMethodConfig: {
+    "payment-details": {
+        setPayment: (_payment: string | undefined) => void;
+        setOnClose: (_listener: (() => void) | undefined) => void;
+    };
+    "account-onboarding": {
+        setFullTermsOfServiceUrl: (_termOfServiceUrl: string | undefined) => void;
+        setRecipientTermsOfServiceUrl: (_recipientTermsOfServiceUrl: string | undefined) => void;
+        setPrivacyPolicyUrl: (_privacyPolicyUrl: string | undefined) => void;
+        setSkipTermsOfServiceCollection: (_skipTermsOfServiceCollection: boolean | undefined) => void;
+        setOnExit: (_listener: (() => void) | undefined) => void;
+    };
+    "issuing-card": {
+        setDefaultCard: (_defaultCard: string | undefined) => void;
+        setCardArtFileLink: (_cardArtFileLink: string | undefined) => void;
+        setCardSwitching: (_cardSwitching: boolean | undefined) => void;
+    };
+    "issuing-cards-list": {
+        setCardArtFileLink: (_cardArtFileLink: string | undefined) => void;
+    };
+};
